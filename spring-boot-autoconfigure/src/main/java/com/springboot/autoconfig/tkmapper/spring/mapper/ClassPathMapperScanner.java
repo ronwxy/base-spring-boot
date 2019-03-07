506823808487396fc8ca9802467933d09e80e8cf
wuxy <ray4work@126.com>
2019-03-07 18:01:56 +0800

    fix mapper useSimpleType

--- spring-boot-autoconfigure/src/main/java/com/springboot/autoconfig/tkmapper/spring/mapper/ClassPathMapperScanner.java
+++ spring-boot-autoconfigure/src/main/java/com/springboot/autoconfig/tkmapper/spring/mapper/ClassPathMapperScanner.java
@@ -15,48 +15,52 @@ import java.util.Set;
 
 public class ClassPathMapperScanner extends org.mybatis.spring.mapper.ClassPathMapperScanner {
 
-	private MapperHelper mapperHelper = new MapperHelper();
+    private MapperHelper mapperHelper = new MapperHelper();
 
-	public ClassPathMapperScanner(BeanDefinitionRegistry registry) {
-		super(registry);
-	}
+    public ClassPathMapperScanner(BeanDefinitionRegistry registry) {
+        super(registry);
+    }
 
-	@Override
-	public Set<BeanDefinitionHolder> doScan(String... basePackages) {
-		Set<BeanDefinitionHolder> beanDefinitions = super.doScan(basePackages);
-		doAfterScan(beanDefinitions);
-		return beanDefinitions;
-	}
+    @Override
+    public Set<BeanDefinitionHolder> doScan(String... basePackages) {
+        Set<BeanDefinitionHolder> beanDefinitions = super.doScan(basePackages);
+        doAfterScan(beanDefinitions);
+        return beanDefinitions;
+    }
 
-	protected void doAfterScan(Set<BeanDefinitionHolder> beanDefinitions) {
-		//如果没有注册过接口，就注册默认的Mapper接口
-		this.mapperHelper.ifEmptyRegisterDefaultInterface();
-		GenericBeanDefinition definition;
-		for (BeanDefinitionHolder holder : beanDefinitions) {
-			definition = (GenericBeanDefinition) holder.getBeanDefinition();
-			if (StringUtil.isNotEmpty(definition.getBeanClassName())
-					&& definition.getBeanClassName().equals("org.mybatis.spring.mapper.MapperFactoryBean")) {
-				definition.setBeanClass(MapperFactoryBean.class);
-				definition.getPropertyValues().add("mapperHelper", this.mapperHelper);
-			}
-		}
-	}
+    protected void doAfterScan(Set<BeanDefinitionHolder> beanDefinitions) {
+        //如果没有注册过接口，就注册默认的Mapper接口
+        this.mapperHelper.ifEmptyRegisterDefaultInterface();
+        GenericBeanDefinition definition;
+        for (BeanDefinitionHolder holder : beanDefinitions) {
+            definition = (GenericBeanDefinition) holder.getBeanDefinition();
+            if (StringUtil.isNotEmpty(definition.getBeanClassName())
+                    && definition.getBeanClassName().equals("org.mybatis.spring.mapper.MapperFactoryBean")) {
+                definition.setBeanClass(MapperFactoryBean.class);
+                definition.getPropertyValues().add("mapperHelper", this.mapperHelper);
+            }
+        }
+    }
 
-	/**
-	 * 从环境变量中获取 mapper 配置信息
-	 *
-	 * @param environment
-	 */
-	public void setMapperProperties(Environment environment) {
-		Config config = SpringBootBindUtil.bind(environment, Config.class, Config.PREFIX);
-		if (config != null) {
-			if (config.getMappers() == null || config.getMappers().isEmpty()) {
-				config.setMappers(Arrays.asList(BaseMapper.class));
-			}
-			if (config.isUseSimpleType()) {
-				config.setUseSimpleType(false);
-			}
-			mapperHelper.setConfig(config);
-		}
-	}
+    /**
+     * 从环境变量中获取 mapper 配置信息
+     *
+     * @param environment
+     */
+    public void setMapperProperties(Environment environment) {
+        Config config = SpringBootBindUtil.bind(environment, Config.class, Config.PREFIX);
+        if (config != null) {
+            if (config.getMappers() == null || config.getMappers().isEmpty()) {
+                config.setMappers(Arrays.asList(BaseMapper.class));
+            }
+            if (config.isUseSimpleType()) {
+                config.setUseSimpleType(false);
+            }
+        } else {
+            config = new Config();
+            config.setUseSimpleType(false);
+            config.setEnumAsSimpleType(true);
+        }
+        mapperHelper.setConfig(config);
+    }
 }
